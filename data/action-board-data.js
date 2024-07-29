e0e4efc0b63a7b620801ce16bd5a59ac3ddf820f
Giorgi Garsevanishvili <george.garsevanidi@gmail.com>
2024-07-29 22:16:14 +0300

    button icons and board sizes

--- data/action-board-data.js
+++ data/action-board-data.js
@@ -1,15 +1,15 @@
 export const actionBoardData = [
   {
     title: 'MOVE FIGURES',
-    icon: 'img'
+    icon: './images/action-board/giza.png'
   },
   {
     title: 'SUMMON FIGURES',
-    icon: 'img'
+    icon: './images/action-board/man.png'
   },
   {
     title: 'GAIN FOLLOWERS',
-    icon: 'img'
+    icon: './images/action-board/pharaoh.png'
   },
   {
     title: 'UNLOCK ANKH POWER',
--- scripts/action-board/action-board.js
+++ scripts/action-board/action-board.js
@@ -11,11 +11,29 @@ export function renderActionBoard () {
         <div class="title"><h5>${item.title}</h5></div>
         <div class="icon-items">
         <button class="icon-button"><img class="icon-button-style" src="${item.icon}" alt=""></button>
+
+        <img class="icon-button-play" src="./images/action-board/play.png" alt="">
+
         <button class="icon-button"><img class="icon-button-style" src="${item.icon}" alt=""></button>
+
+        <img class="icon-button-play" src="./images/action-board/play.png" alt="">
+
         <button class="icon-button"><img class="icon-button-style" src="${item.icon}" alt=""></button>
+
+        <img class="icon-button-play" src="./images/action-board/play.png" alt="">
+        
         <button class="icon-button"><img class="icon-button-style" src="${item.icon}" alt=""></button>
+
+        <img class="icon-button-play" src="./images/action-board/play.png" alt="">
+
         <button class="icon-button"><img class="icon-button-style" src="${item.icon}" alt=""></button>
+
+        <img class="icon-button-play" src="./images/action-board/play.png" alt="">
+
         <button class="icon-button"><img class="icon-button-style" src="${item.icon}" alt=""></button>
+
+        <img class="icon-button-play" src="./images/action-board/play.png" alt="">
+        
         <button class="icon-button-last"><img class="icon-button-style" src="${item.icon}" alt=""></button>
         </div>
       </div>
