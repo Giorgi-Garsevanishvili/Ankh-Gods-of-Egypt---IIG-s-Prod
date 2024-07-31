4a5f802092f4a50d3785836c512068eff88bad33
Giorgi Garsevanishvili <george.garsevanidi@gmail.com>
2024-07-31 13:51:32 +0300

    added undo and reset buttons, function for close circuite switcher on event board

--- data/action-board-data.js
+++ data/action-board-data.js
@@ -2,23 +2,27 @@ export const actionBoardData = [
   {
     title: 'MOVE FIGURES',
     icon: './images/action-board/giza.png',
-    styleLink: "MF"
+    styleLink: "MF",
+    id: 0
+    
   },
   {
     title: 'SUMMON FIGURES',
     icon: './images/action-board/man.png',
-    styleLink: "SF"
+    styleLink: "SF",
+    id: 1
   },
   {
     title: 'GAIN FOLLOWERS',
     icon: './images/action-board/pharaoh.png',
-    styleLink: "GF"
+    styleLink: "GF",
+    id: 2
   },
   {
     title: 'UNLOCK ANKH POWER',
     icon: './images/action-board/ankh.png',
-    styleLink: "UAP"
-
+    styleLink: "UAP",
+    id: 3
   }
 ];
 
@@ -29,6 +33,14 @@ export const EventBoardData = [
       primary: 'first',
       secondary: ''
     },
+    id: 0
+  },
+  {
+    icon: './images/action-board/pyramid.png',
+    styleLink: {
+      primary: 'pyramid',
+      secondary: 'middle'
+    },
     id: 1
   },
   {
@@ -36,125 +48,135 @@ export const EventBoardData = [
     styleLink: {
       primary: 'pyramid',
       secondary: 'middle'
-    }
+    },
+    id: 2
   },
   {
     icon: './images/action-board/pyramid.png',
     styleLink: {
       primary: 'pyramid',
       secondary: 'middle'
-    }
-  },
-  {
-    icon: './images/action-board/pyramid.png',
-    styleLink: {
-      primary: 'pyramid',
-      secondary: 'middle'
-    }
+    },
+    id: 3
   },
   {
     icon: './images/action-board/fan.png',
     styleLink: {
       primary: 'fan',
       secondary: 'middle'
-    }
+    },
+    id: 4
   },
   {
     icon: './images/action-board/camel.png',
     styleLink: {
       primary: 'camel',
       secondary: 'middle'
-    }
+    },
+    id: 5
   },
   {
     icon: './images/action-board/pyramid.png',
     styleLink: {
       primary: 'pyramid',
       secondary: 'middle'
-    }
+    },
+    id: 6
   },
   {
     icon: './images/action-board/pyramid.png',
     styleLink: {
       primary: 'pyramid',
       secondary: 'middle'
-    }
+    },
+    id: 7
   },
   {
     icon: './images/action-board/fan.png',
     styleLink: {
       primary: 'fan',
       secondary: 'middle'
-    }
+    },
+    id: 8
   },
   {
     icon: './images/action-board/camel.png',
     styleLink: {
       primary: 'camel',
       secondary: 'middle'
-    }
+    },
+    id: 9
   },
   {
     icon: './images/action-board/pyramid.png',
     styleLink: {
       primary: 'pyramid',
       secondary: 'middle'
-    }
+    },
+    id: 10
   },
   {
     icon: './images/action-board/pyramid.png',
     styleLink: {
       primary: 'pyramid',
       secondary: 'middle'
-    }
+    },
+    id: 11
   },
   {
     icon: './images/action-board/fan.png',
     styleLink: {
       primary: 'fan',
       secondary: 'middle'
-    }
+    },
+    id: 12
   },
   {
     icon: './images/action-board/camel.png',
     styleLink: {
       primary: 'camel',
       secondary: 'middle'
-    }
+    },
+    id: 13
   },
   {
     icon: './images/action-board/pyramid.png',
     styleLink: {
       primary: 'pyramid',
       secondary: 'middle'
-    }
+    },
+    id: 14
   },
   {
     icon: './images/action-board/pyramid.png',
     styleLink: {
       primary: 'pyramid',
       secondary: 'middle'
-    }
+    },
+    id: 15
   },
   {
     icon: './images/action-board/fan.png',
     styleLink: {
       primary: 'fan',
       secondary: 'middle'
-    }
+    },
+    id: 16
   },
   {
     icon: './images/action-board/pyramid.png',
     styleLink: {
       primary: 'pyramid',
       secondary: 'middle'
-    }
+    },
+    id: 17
   },
   {
     icon: './images/action-board/fan.png',
     styleLink: {
       primary: 'fan',
       secondary: 'last'
-    }
+    },
+    id: 18
   }
 ]
--- scripts/action-board/action-board.js
+++ scripts/action-board/action-board.js
@@ -1,8 +1,8 @@
-import { actionBoardData, EventBoardData } from "../../data/action-board-data.js"
+import { actionBoardData, EventBoardData } from "../../data/action-board-data.js";
 
-renderActionBoard();
+let activeButtonIndex = 0; 
 
-export function renderActionBoard () {
+function renderActionBoard() {
   let boardHTML = document.querySelector('.board-html');
   let actionBoardHTML = '';
   let eventBoardHTML = '';
@@ -10,116 +10,103 @@ export function renderActionBoard () {
   EventBoardData.forEach((item) => {
     eventBoardHTML += `
     <div class="event-board">
-      <div class="event-button event-button-${item.styleLink.primary}"><img class="icon-button-style" src="${item.icon}" alt=""></button></div>
+      <div id="${item.id}" class="event-button event-button-${item.styleLink.primary}" data-id="${item.id}">
+        <img class="icon-button-style" src="${item.icon}" alt="">
+      </div>
     </div>
     <img class="icon-button-play-event arrow-${item.styleLink.secondary}" src="./images/action-board/play.png" alt="">
-    `
-  })
-
-  
+    `;
+  });
 
   actionBoardData.forEach((item) => {
-    actionBoardHTML +=  `
-    <div class="actions-board">
+    actionBoardHTML += `
+    <div id="${item.id}" class="actions-board">
       <div class="action-square">
         <div class="figures">
           <div class="title-image"></div>
           <div class="title"><h5>${item.title}</h5></div>
           <div class="icon-items">
-
-          <button class="icon-button icon-button-${item.styleLink}"><img class="icon-button-style" src="${item.icon}" alt=""></button>
-
-          <img class="icon-button-play" src="./images/action-board/play.png" alt="">
-
-          <button class="icon-button icon-button-${item.styleLink}"><img class="icon-button-style" src="${item.icon}" alt=""></button>
-
-          <img class="icon-button-play" src="./images/action-board/play.png" alt="">
-
-          <button class="icon-button icon-button-${item.styleLink}"><img class="icon-button-style" src="${item.icon}" alt=""></button>
-
-          <img class="icon-button-play" src="./images/action-board/play.png" alt="">
-          
-          <button class="icon-button icon-button-${item.styleLink}"><img class="icon-button-style" src="${item.icon}" alt=""></button>
-
-          <img class="icon-button-play" src="./images/action-board/play.png" alt="">
-
-          <button class="icon-button icon-button-${item.styleLink}"><img class="icon-button-style" src="${item.icon}" alt=""></button>
-
-          <img class="icon-button-play" src="./images/action-board/play.png" alt="">
-
-          <button class="icon-button icon-button-${item.styleLink}"><img class="icon-button-style" src="${item.icon}" alt=""></button>
-
-          <img class="icon-button-play" src="./images/action-board/play.png" alt="">
-          
-          <button class="icon-button icon-button-${item.styleLink} icon-button-last last-${item.styleLink}"><img class="icon-button-style icon-button-style " src="${item.icon}" alt=""></button>
-
-          <img class="icon-button-play-${item.styleLink}" src="./images/action-board/play.png" alt="">
+            ${[...Array(6)].map(() => `
+              <button class="icon-button icon-button-${item.styleLink}" data-id="${item.id}">
+                <img class="icon-button-style" src="${item.icon}" alt="">
+              </button>
+              <img class="icon-button-play" src="./images/action-board/play.png" alt="">
+            `).join('')}
+            <button class="icon-button icon-button-${item.styleLink} icon-button-last last-${item.styleLink}">
+              <img class="icon-button-style" src="${item.icon}" alt="">
+            </button>
+            <img class="icon-button-play-${item.styleLink}" src="./images/action-board/play.png" alt="">
           </div>
         </div>
       </div>
-     </div> 
-    `;
+    </div>`;
   });
 
   boardHTML.innerHTML = `
   <div class="action-board-container">${actionBoardHTML}</div>
   <div class="event-board-container hidden">${eventBoardHTML}</div>
-`;
+  `;
 
-const firtsButton = document.querySelector('.event-button-first');
-  firtsButton.classList.add('on');
+  const buttons = document.querySelectorAll('.next-button, .prev-button');
+  const eventButtons = document.querySelectorAll('.event-button');
 
-const buttons = document.querySelectorAll('.next-button, .prev-button');
-
-const eventButton = document.querySelectorAll('.event-button');
-
-function keepIconON (event){
-  const eventButton = event.currentTarget;
-  EventBoardData.forEach(item => {
-    let buttonsid = item.id
-
+  function switchToNextButton() {
+    const totalButtons = eventButtons.length;
     
-  if (firtsButton.classList.contains('on') && buttonsid === 1){
-    eventButton.classList.add('on')
-    firtsButton.classList.remove('on')
-    buttonsid = 2;
-  } else if (buttonsid === 1){
-    eventButton.classList.add('on')
+    // Remove 'active' class from the current button
+    eventButtons[activeButtonIndex].classList.remove('on');
+    
+    // Update the index of the active button
+    activeButtonIndex = (activeButtonIndex + 1) % totalButtons; 
+    
+    // Add 'active' class to the new active button
+    eventButtons[activeButtonIndex].classList.add('on');
+    
+    // Update the button's ID to manage 'disabled' state correctly
+    eventButtons.forEach((button, index) => {
+      button.disabled = (index !== activeButtonIndex);
 
+      console.log(activeButtonIndex);
+      console.log(totalButtons);
+    });
+
+    if (activeButtonIndex === totalButtons - 1 ) {
+      alert('END OF THE MATCH')
+    }
   }
 
-}
+  function toggleBoard() {
+    const actionBoardContainer = document.querySelector('.action-board-container');
+    const eventBoardContainer = document.querySelector('.event-board-container');
+    const downArrowSection = document.querySelector('.down-arrow-section');
     
-  )};
-
-  
-
-
-function toggleBoard() {
-  const actionBoardContainer = document.querySelector('.action-board-container');
-  const eventBoardContainer = document.querySelector('.event-board-container');
-  const downArrowSection = document.querySelector('.down-arrow-section');
-  
-  if (actionBoardContainer.classList.contains('hidden')) {
-    actionBoardContainer.classList.remove('hidden');
-    eventBoardContainer.classList.add('hidden');
-    downArrowSection.classList.remove('hidden');
-    document.querySelector('.page-title').textContent = 'Action Board';
-  } else {
-    actionBoardContainer.classList.add('hidden');
-    eventBoardContainer.classList.remove('hidden');
-    downArrowSection.classList.add('hidden');
-    document.querySelector('.page-title').textContent = 'Event Board';
+    if (actionBoardContainer.classList.contains('hidden')) {
+      actionBoardContainer.classList.remove('hidden');
+      eventBoardContainer.classList.add('hidden');
+      downArrowSection.classList.remove('hidden');
+      document.querySelector('.page-title').textContent = 'Action Board';
+    } else {
+      actionBoardContainer.classList.add('hidden');
+      eventBoardContainer.classList.remove('hidden');
+      downArrowSection.classList.add('hidden');
+      document.querySelector('.page-title').textContent = 'Event Board';
+    }
   }
 
-  
+  buttons.forEach(button => button.addEventListener('click', toggleBoard));
+
+  // Initialize the first button as active
+  if (eventButtons.length > 0) {
+    eventButtons[activeButtonIndex].classList.add('on');
+    eventButtons.forEach((button, index) => {
+      button.disabled = (index !== activeButtonIndex);
+    });
+  }
+ 
+  // Add event listener for button switching
+  eventButtons.forEach(button => {
+    button.addEventListener('click', switchToNextButton);
+  });
 }
 
-buttons.forEach(button => button.addEventListener('click', toggleBoard));
-
-eventButton.forEach(item => item.addEventListener('click',
-  keepIconON));
-}
-
-
-
+renderActionBoard();
