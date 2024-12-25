2ef248107fd80eb07083a8a14112984ef022106b
Giorgi Garsevanishvili <george.garsevanidi@gmail.com>
2024-12-25 20:05:56 +0200

    test sounds added

--- tools/display-message.js
+++ tools/display-message.js
@@ -1,11 +1,15 @@
 export function displayMessage (messageText, timer) {
+  const sound = document.getElementById('notification-sound');
   const messageBox = document.querySelector('.blur');
   const message = document.querySelector('.message');
 
+  
   message.textContent = messageText;
   
   messageBox.classList.remove('hidden');
+  sound.play();
   setTimeout(() => {
     messageBox.classList.add('hidden');
+    sound.pause(); 
   }, timer);
 }
